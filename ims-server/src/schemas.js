06e36a1edaea01c0d23989fe488695753dbdd076
CreativeCookieConnections <creativecookieconnections@gmail.com>
2026-07-08 20:53:16 -0500

    Built the addItemSchema

--- ims-server/src/schemas.js
+++ ims-server/src/schemas.js
@@ -5,4 +5,21 @@
  * Description: Validate APIs 
  */
 
-// Add a schema for creating an inventory item
+// Add a validation schema for adding new Item Inventory to the Inventory Management System (IMS)
+const addItemSchema = {
+    type: 'object',
+    properties: {
+        categoryId: { type: 'number', required: true },
+        supplierId: { type: 'number', required: true },
+        name: { type: 'string', minLength: 2, maxLength: 100, required: true },
+        description: { type: 'string', maxLength: 250 },
+        quantity: { type: 'number', minimum: 0, required: true },
+        price: { type: 'number', minimum: 0, required: true },
+
+    }
+};
+
+module.exports = {
+    addItemSchema
+};
+
